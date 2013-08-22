a5ced0d1a68c9a6c38b3820450a0b92b14379262
Chris Eich <cjeich@gmail.com>
2013-08-22 04:21:55 -0700

    Mostly static pages end

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,33 +1,43 @@
 require 'spec_helper'
 
-describe "Static Pages" do
-  
-	# Home Page Tests
-	describe "Home Page" do
+describe "Static pages" do
 
-		it "Should have the content 'Sample App'" do
-			visit '/static_pages/home'
-			expect(page).to have_content('Sample App')
-		end
-	end
+  describe "Home page" do
 
-	# Help Page Tests
-	describe "Help Page" do
-	
-		it "Should have the content 'Help'" do
-			visit '/static_pages/help'
-			expect(page).to have_content('Help')
-		end
-	
-	end
+    it "should have the content 'Sample App'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Sample App')
+    end
 
-	describe "About Page" do
-	
-		it "Should have the content 'About Us'" do
-			visit '/static_pages/about'
-			expect(page).to have_content('About Us')
-		end
-	
-	end
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+    end
+  end
 
+  describe "Help page" do
+
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+    end
+
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+    end
+  end
+
+  describe "About page" do
+
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
+    end
+
+    it "should have the title 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+    end
+  end
 end
